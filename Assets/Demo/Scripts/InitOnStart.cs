qiankanglai/LoopScrollRect

commit f0bca407df5367090ce52b9d998f740519486a11

move demo related scripts together and tidy up namespace

Assets/Demo/Scripts/InitOnStart.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine.UI;
 
-namespace SG
+namespace Demo
 {
     [RequireComponent(typeof(UnityEngine.UI.LoopScrollRect))]
     [DisallowMultipleComponent]

Assets/Demo/Scripts/BaseListBank.cs
@@ -2,7 +2,7 @@
  */
 using UnityEngine;
 
-namespace qiankanglai.LoopScrollRect
+namespace Demo
 {
     /* The base class of the list content container
      *

Assets/Demo/Scripts/CustomListBank.cs
@@ -1,72 +1,74 @@
-﻿using qiankanglai.LoopScrollRect;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class CustomListBank : BaseListBank
+namespace Demo
 {
-    private List<int> contents = new List<int>{
-            1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-            11, 12, 13, 14, 15, 16, 17, 18, 19, 20
+    public class CustomListBank : BaseListBank
+    {
+        private List<int> contents = new List<int>{
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                11, 12, 13, 14, 15, 16, 17, 18, 19, 20
+            };
+
+        // Cell Sizes
+        public List<Vector2> m_CellSizes = new List<Vector2> 
+        {
+            new Vector2(120, 120),
+            new Vector2(170, 120),
+            new Vector2(220, 120)
         };
 
-    // Cell Sizes
-    public List<Vector2> m_CellSizes = new List<Vector2> 
-    {
-        new Vector2(120, 120),
-        new Vector2(170, 120),
-        new Vector2(220, 120)
-    };
-
-    public override object GetListContent(int index)
-    {
-        return contents[index];
-    }
-
-    public override int GetListLength()
-    {
-        return contents.Count;
-    }
-
-    public override int GetCellPreferredTypeIndex(int index)
-    {
-        var TempConten = GetListContent(index);
-
-        int TempData = (int)TempConten;
-        int ResultIndex = Mathf.Abs(TempData) % 3;
-
-        return ResultIndex;
-    }
-
-    public override Vector2 GetCellPreferredSize(int index)
-    {
-        int ResultIndex = GetCellPreferredTypeIndex(index);
-
-        Vector2 FinalValue = m_CellSizes[ResultIndex];
-
-        return FinalValue;
-    }
-
-    public void AddContent(int newContent)
-    {
-        contents.Add(newContent);
-    }
-
-    public void DelContentByIndex(int index)
-    {
-        if (contents.Count <= index)
+        public override object GetListContent(int index)
         {
-            return;
+            return contents[index];
         }
-        contents.RemoveAt(index);
-    }
 
-    public void SetContents(List<int> newContents)
-    {
-        contents = newContents;
-    }
+        public override int GetListLength()
+        {
+            return contents.Count;
+        }
 
-    public List<int> GetContents()
-    {
-        return contents;
+        public override int GetCellPreferredTypeIndex(int index)
+        {
+            var TempConten = GetListContent(index);
+
+            int TempData = (int)TempConten;
+            int ResultIndex = Mathf.Abs(TempData) % 3;
+
+            return ResultIndex;
+        }
+
+        public override Vector2 GetCellPreferredSize(int index)
+        {
+            int ResultIndex = GetCellPreferredTypeIndex(index);
+
+            Vector2 FinalValue = m_CellSizes[ResultIndex];
+
+            return FinalValue;
+        }
+
+        public void AddContent(int newContent)
+        {
+            contents.Add(newContent);
+        }
+
+        public void DelContentByIndex(int index)
+        {
+            if (contents.Count <= index)
+            {
+                return;
+            }
+            contents.RemoveAt(index);
+        }
+
+        public void SetContents(List<int> newContents)
+        {
+            contents = newContents;
+        }
+
+        public List<int> GetContents()
+        {
+            return contents;
+        }
     }
 }

Assets/Demo/Scripts/InitOnStart_Custom.cs
@@ -1,9 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 using UnityEngine.UI;
-using qiankanglai.LoopScrollRect;
 
-namespace SG
+namespace Demo
 {
     [RequireComponent(typeof(UnityEngine.UI.LoopScrollRect))]
     [DisallowMultipleComponent]

Assets/Demo/Scripts/ScrollIndexCallbackBase.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-namespace qiankanglai.LoopScrollRect
+namespace Demo
 {
     public class ScrollIndexCallbackBase : MonoBehaviour
     {

Assets/Demo/Scripts/ScrollIndexCallback_Custom.cs
@@ -1,26 +1,28 @@
-﻿using qiankanglai.LoopScrollRect;
-using UnityEngine.UI;
+﻿using UnityEngine.UI;
 
-public class ScrollIndexCallback_Custom : ScrollIndexCallbackBase
+namespace Demo
 {
-    public Text m_CellText;
-    public Text m_ValueText;
-
-    public override void ScrollCellIndex(int idx, object content)
+    public class ScrollIndexCallback_Custom : ScrollIndexCallbackBase
     {
-        base.ScrollCellIndex(idx, content);
+        public Text m_CellText;
+        public Text m_ValueText;
 
-        var Tempcontent = (int)content;
-
-        string name = "Cell " + idx.ToString();
-        if (m_CellText != null)
+        public override void ScrollCellIndex(int idx, object content)
         {
-            m_CellText.text = name;
-        }
+            base.ScrollCellIndex(idx, content);
 
-        if(m_ValueText != null)
-        {
-            m_ValueText.text = string.Format("Value: {0}", Tempcontent);
+            var Tempcontent = (int)content;
+
+            string name = "Cell " + idx.ToString();
+            if (m_CellText != null)
+            {
+                m_CellText.text = name;
+            }
+
+            if(m_ValueText != null)
+            {
+                m_ValueText.text = string.Format("Value: {0}", Tempcontent);
+            }
         }
     }
 }

commit a5e7f76ad99a9f2d51dd18ed588f5f7c0c13419b

Fix DemoScene sample, broken after f0bca40 split multi prefab LoopScrollRect from old style (just keep API stable)

Assets/Demo/Scripts/InitOnStart.cs
@@ -6,7 +6,7 @@ namespace Demo
 {
     [RequireComponent(typeof(UnityEngine.UI.LoopScrollRect))]
     [DisallowMultipleComponent]
-    public class InitOnStart : MonoBehaviour, LoopScrollPrefabSource, LoopScrollMultiDataSource
+    public class InitOnStart : MonoBehaviour, LoopScrollPrefabSource, LoopScrollDataSource
     {
         public GameObject item;
         public int totalCount = -1;
@@ -40,7 +40,7 @@ namespace Demo
 
         void Start()
         {
-            var ls = GetComponent<LoopScrollRectMulti>();
+            var ls = GetComponent<LoopScrollRect>();
             ls.prefabSource = this;
             ls.dataSource = this;
             ls.totalCount = totalCount;

Assets/Scripts/LoopScrollDataSource.cs
@@ -5,6 +5,6 @@ namespace UnityEngine.UI
 {
     public interface LoopScrollDataSource
     {
-        void ProvideData(Transform transform);
+        void ProvideData(Transform transform, int idx);
     }
 }

Assets/Scripts/LoopScrollRect.cs
@@ -15,7 +15,7 @@ namespace UnityEngine.UI
         
         protected override void ProvideData(Transform transform, int index)
         {
-            dataSource.ProvideData(transform);
+            dataSource.ProvideData(transform, index);
         }
         
         protected override RectTransform GetFromTempPool(int itemIdx)