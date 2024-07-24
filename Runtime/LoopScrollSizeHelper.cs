af53abe68950f7f0634065a99d0cf8c034b1b169
liaoqiang <liaoqiang@nibirutech.com>
2024-07-24 09:55:08 +0800

    分拆相关代码

--- Runtime/LoopScrollSizeHelper.cs
+++ Runtime/LoopScrollSizeHelper.cs
@@ -8,37 +8,4 @@ namespace UnityEngine.UI
     {
         Vector2 GetItemsSize(int itemsCount);
     }
-
-    public static class LoopScrollSizeUtils
-    {
-        public static float GetPreferredHeight(RectTransform item)
-        {
-            ILayoutElement minLayoutElement;
-            ILayoutElement maxLayoutElement;
-            var minHeight = LayoutUtility.GetLayoutProperty(item, e => e.minHeight, 0, out minLayoutElement);
-            var preferredHeight = LayoutUtility.GetLayoutProperty(item, e => e.preferredHeight, 0, out maxLayoutElement);
-            var result = Mathf.Max(minHeight, preferredHeight);
-            if (maxLayoutElement == null && minLayoutElement == null)
-            {
-                result = item.rect.height;
-            }
-
-            return result;
-        }
-        
-        public static float GetPreferredWidth(RectTransform item)
-        {
-            ILayoutElement minLayoutElement;
-            ILayoutElement maxLayoutElement;
-            var minWidth = LayoutUtility.GetLayoutProperty(item, e => e.minWidth, 0, out minLayoutElement);
-            var preferredWidth = LayoutUtility.GetLayoutProperty(item, e => e.preferredWidth, 0, out maxLayoutElement);
-            var result = Mathf.Max(minWidth, preferredWidth);
-            if (maxLayoutElement == null && minLayoutElement == null)
-            {
-                result = item.rect.width;
-            }
-
-            return result;
-        }
-    }
 }
